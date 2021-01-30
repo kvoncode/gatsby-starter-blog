1bb03f8401ac003f7a835af6d3d5ef1e5a32a8dd
kvoncode <kvoncode@gmail.com>
2021-01-30 18:06:50 +0900

    add: styled components

--- src/pages/sidenav.jsx
+++ src/pages/sidenav.jsx
@@ -1,18 +1,33 @@
 import React from "react"
+import styled from "styled-components"
+
+const CloseMenu = styled.button`
+  background-color: red;
+  border: 1px solid red;
+`
+
+const SideNav = styled.aside`
+  background-color: green;
+
+  @media (max-width: 540px) {
+    visibility: hidden;
+    background-color: purple;
+  }
+`
 
 const Sidenav = () => {
   return (
     <>
       <a href="#sidenav-open">Open</a>
-      <button
+      <CloseMenu
         href="#"
         id="#sidenav-close"
         title="Close menu"
         aria-label="Close Menu"
       >
         Close
-      </button>
-      <aside id="#sidenav-open">Hidden text</aside>
+      </CloseMenu>
+      <SideNav id="#sidenav-open">Hidden text</SideNav>
     </>
   )
 }
